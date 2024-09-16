c569f3b2af1d42e5e22c8de9acb554f22109e79a
metu dein <maxmetudein.com>
2024-09-16 18:02:11 +0100

    update

--- app/api/auth/logout/route.js
+++
@@ -1,8 +0,0 @@
-import { signOut } from "next-auth/react"
-import { NextResponse } from "next/server"
-
-
-export const GET = async (request) => {
-    await signOut()
-    return NextResponse.json({ success: 'signed out' }, { status: 200 })
-} 
